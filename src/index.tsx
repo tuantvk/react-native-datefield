1ed44c78e84edc9b5d57ddbb7901d0dce15879b9
tuantv <tuanhanhphuc123@gmail.com>
2021-04-19 09:21:15 +0700

    fix: import type from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import MonthDateYearField from './MonthDateYearField';
 import YearMonthDateField from './YearMonthDateField';
-import type { DateFieldProps } from './types';
+import { DateFieldProps } from './types';
 
 export { DateFieldProps, MonthDateYearField, YearMonthDateField };
 export { default } from './DateMonthYearField';
